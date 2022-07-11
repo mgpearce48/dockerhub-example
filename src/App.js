92e2212f2ada87fc1a7630168f049f021bc8b082
Michael Pearce <mgpearce48@gmail.com>
2022-07-11 16:06:21 +1000

    Change source files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-import React, { Suspense } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import logo from './logo.svg';
+import './App.css';
 
-import Layout from "./components/Layout/Layout";
-import LoadingSpinner from "./components/UI/LoadingSpinner";
-
-const AuthPage = React.lazy(() => import("./pages/AuthPage"));
-const MainPage = React.lazy(() => import("./pages/MainPage"));
-const NotesPage = React.lazy(() => import("./pages/NotesPage"));
-const LaunchDatePage = React.lazy(() => import("./pages/LaunchDatePage"));
-
-const App = () => {
-	const { isLoggedIn } = useSelector((state) => state.auth);
-
-	return (
-		<Layout>
-			<Suspense
-				fallback={
-					<div className="centered">
-						<LoadingSpinner />
-					</div>
-				}
-			>
-				<Routes>
-					<Route path="/" element={<MainPage />} />
-					{!isLoggedIn && <Route path="/auth" element={<AuthPage />} />}
-					<Route
-						path="/notes"
-						element={isLoggedIn ? <NotesPage /> : <AuthPage />}
-					/>
-					<Route
-						path="/launchdate"
-						element={isLoggedIn ? <LaunchDatePage /> : <AuthPage />}
-					/>
-					<Route path="*" element={<Navigate to="/" />} />
-				</Routes>
-			</Suspense>
-		</Layout>
-	);
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- src/components/LaunchDate/LaunchDateForm.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./LaunchDateForm.module.css";
-
-const LaunchDateForm = () => {
-	return <p>LaunchDate Form</p>;
-};
-
-export default LaunchDateForm;
